72140dbfe77b98199b4f0e13b646ed200bc6fadc
Gary Lu <madcated.lu@gmail.com>
2017-09-17 20:18:22 -0600

    Lynda-Learning Python-Chap5.Working with Wed Data

---
+++ Lynda_Learning Python/Exercise Files/Ch5/5_1_inetdata.py
@@ -0,0 +1,16 @@
+# 
+# Example file for retrieving data from the internet
+# (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
+import urllib.request
+
+def main():
+  # open a connection to a URL using urllib2
+  webUrl = urllib.request.urlopen("http://google.com")
+  # get the result code and print it
+  print ("result code: " + str(webUrl.getcode()))
+  # read the data from the URL and print it
+  data = webUrl.read()#.decode("utf-8") # decode the data as UTF-8
+  print (data)
+
+if __name__ == "__main__":
+  main()
