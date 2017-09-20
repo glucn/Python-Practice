42a80418fe42495fca3e52d6ea782ea4878a372f
Gary Lu <madcated.lu@gmail.com>
2017-09-20 00:33:53 -0600

    Lynda-Programming Foundations: Real-World Examples-Chap2-5

---
+++ Lynda_Programming Foundation Real-World Examples/Exercise Files/Ch02/02_03_naming_shirts.py
@@ -0,0 +1,31 @@
+""" Two Names, One Shirt """
+
+class shirt:
+
+    def __init__(self):
+        self.clean = True
+
+    def make_dirty(self):
+        self.clean = False
+
+    def make_clean(self):
+        self.clean = True
+
+red = shirt()
+crimson = red
+
+print(id(red))
+print(id(crimson))
+print(red.clean)
+print(crimson.clean)
+
+red.make_dirty()
+print(red.clean)
+print(crimson.clean)
+
+print (red is crimson)
+
+crimson = shirt()
+print(id(red))
+print(id(crimson))
+print (red is crimson)
